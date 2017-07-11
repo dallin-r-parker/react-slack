9869c8fe199615c985dbf6ff6cf5e1b9d7da3c0d
Dallin Parker <dallin.r.parker@gmail.com>
2017-07-10 21:25:29 -0600

    set up react and redux

---
+++ src/redux/reducer.js
@@ -0,0 +1,8 @@
+const initialState = {
+	channels: ['home', 'random', 'working', 'devTeam'],
+	directChannels: ['Dallin', 'Cameron', 'Mikayda']
+}
+
+export default function reducer(state=initialState, action){
+	return state;
+}
