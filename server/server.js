b2731c8d4d3d78e9682f5a621af19e9224806b0c
Dallin Parker <dallin.r.parker@gmail.com>
2017-07-17 20:36:48 -0600

    set up local db connection with massiv

--- server/server.js
+++ server/server.js
@@ -1,14 +1,42 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+require('dotenv').config()
 const cors = require('cors')
-const port = 8080;
+const massive = require('massive')
+const express = require('express')
+const session = require('express-session')
+const bodyParser = require('body-parser')
 
-const app = express();
+
+// INITIATE EXPRESS APP & SET LISTENING PORT ================
+const app = module.exports = express();
+app.set('port', process.env.PORT || 8080)
+
+// MIDDLEWARE FOR EVERYTHING TO PASS THROUGH ================
 app.use(cors())
 app.use(bodyParser.json())
 
+// PASSPORT STRATEGY ===================================
+const passport = require('./controllers/auth/passport')
+
+// MASSIVE DB ==========================================
+massive(process.env.DB_CONNECTION).then(db => {
+	app.set('db', db)
+}).catch((err) => (console.log("massive DB Error: ", err)))
 
 const channels = ['lead', 'random', 'working', 'devTeam'];
+
+// MIDDLEWARE POLICY ===================================
+const checkAuthed = (req, res, next) => {
+	if(!req.isAuthenticated()) return res.status(401).send("Unauthorized")
+	return next()
+};
+
+// EXPRESS SESSIONS =====================================
+app.use(session({
+	saveUninitialized: false,
+	resave: false
+}));
+app.use(passport.initialize())
+app.use(passport.session())
 
 
 app.get('/api/channels', (req, res, next) => {
@@ -23,6 +51,6 @@ app.post('/api/login', (req, res, next) => {
 })
 
 
-app.listen(port, () => {
-	console.log(`listening on ${port}`)
+app.listen(app.get('port'), () => {
+	console.log('listening on: ', app.get('port'))
 })
