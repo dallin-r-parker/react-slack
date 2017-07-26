95945f93fdfd5f8915d08d86604163e8e5e34fe3
Dallin Parker <dallin.r.parker@gmail.com>
2017-07-25 23:36:59 -0600

    added cors header setting to backend through middleware

--- server/controllers/encrypt.js
+++ server/controllers/encrypt.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs')
 
 exports.hashPW = (password) => {
 	let salt = bcrypt.genSaltSync(10);
+	//noinspection UnnecessaryLocalVariableJS
 	let hash = bcrypt.hashSync(password, salt);
 	return hash;
 };
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const massive = require('massive')
 const express = require('express')
 const session = require('express-session')
 const bodyParser = require('body-parser')
+const {corsHeaders} = require('./middleware/middleware')
 
 
 // INITIATE EXPRESS APP & SET LISTENING PORT ================
@@ -13,7 +14,7 @@ app.set('port', process.env.PORT || 5050)
 // MIDDLEWARE FOR EVERYTHING TO PASS THROUGH ================
 app.use(cors())
 app.use(bodyParser.json())
-
+app.use(corsHeaders)
 // MASSIVE DB ==========================================
 massive(process.env.DB_CONNECTION).then(db => {
 	app.set('db', db)
