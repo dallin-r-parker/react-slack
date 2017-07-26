5c5ce43a47a186875ccccf73228dd811d02dc34a
Dallin Parker <dallin.r.parker@gmail.com>
2017-07-25 23:28:15 -0600

    have authentication and routing done

--- src/index.js
+++ src/index.js
@@ -4,26 +4,17 @@ import {HashRouter, Route, Switch} from 'react-router-dom'
 import {Provider} from 'react-redux'
 import store from './redux/store'
 import './index.css'
+import LoginContainer from './Routes'
 import App from './App'
-import Login from './components/Login/Login/Login'
-import Root from './Root'
-import registerServiceWorker from './registerServiceWorker'
-
 
 ReactDOM.render(
-	<Provider store={store}>
-		<HashRouter>
-				<div>
-					<Route exact path="/" component={Root}/>
-					<Switch>
-						<Route exact path="/home" component={App}/>
-					</Switch>
-					<Switch>
-						<Route exact path="/login" component={Login}/>
-					</Switch>
-				</div>
-		</HashRouter>
-	</Provider>,
+	<HashRouter>
+		<Provider store={store}>
+			<Switch>
+				<Route exact path="/" component={LoginContainer}/>
+				<Route path="/home" component={App}/>
+			</Switch>
+		</Provider>
+	</HashRouter>,
 	document.getElementById('root'))
-registerServiceWorker()
 
