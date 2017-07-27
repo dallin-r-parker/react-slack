11e78a4ce5b94680b08ed70d1a53e46cac399593
Dallin Parker <dallin.r.parker@gmail.com>
2017-07-27 12:28:37 -0600

    event listeners are all set up for Registering account

--- src/components/Login/Register/Register.js
+++ src/components/Login/Register/Register.js
@@ -6,6 +6,32 @@ import './Register.css'
 export  default class Register extends Component{
 	constructor(props){
 		super(props)
+
+		this.handleFirst = this.handleFirst.bind(this)
+		this.handleLast = this.handleLast.bind(this)
+		this.handleEmail = this.handleEmail.bind(this)
+		this.handlePw = this.handlePw.bind(this)
+		this.handleSubmit = this.handleSubmit.bind(this)
+	}
+
+	handleFirst(value){
+		console.log("First2: ", value)
+	}
+
+	handleLast(value){
+		console.log("Last2: ", value)
+	}
+
+	handleEmail(value){
+		console.log("Email2: ", value)
+	}
+
+	handlePw(value){
+		console.log("PW2: ", value)
+	}
+
+	handleSubmit(){
+
 	}
 
 	render(){
@@ -13,7 +39,11 @@ export  default class Register extends Component{
 			<div className="register-container">
 				<LoginHeader/>
 				<div className="register-input-wrap">
-					<RegisterInput/>
+					<RegisterInput
+						first={this.handleFirst}
+						last={this.handleLast}
+						email={this.handleEmail}
+						password={this.handlePw}/>
 				</div>
 			</div>
 		)
--- src/components/Login/RegisterInput/RegisterInput.js
+++ src/components/Login/RegisterInput/RegisterInput.js
@@ -1,18 +1,43 @@
 import React from 'react'
 import './RegisterInput.css'
+import {Link} from 'react-router-dom'
 
 export default function RegisterInput(props) {
+	console.log(props)
+	function handleFirst(e) {
+		props.first(e.target.value)
+	}
+	
+	function handleLast(e) {
+		props.last(e.target.value)
+	}
+	
+	function handleEmail(e){
+		props.email(e.target.value)
+	}
+	
+	function handlePw(e){
+		props.password(e.target.value)
+	}
+	
+	function handleSubmit() {
+		
+	}
 
 	return(
 		<section className="register-input">
 			<h1>Sign Up</h1>
-			<h3>Enter in applicable information</h3>
-			<input type="text" placeholder="first name"/>
-			<input type="text" placeholder="last name"/>
-			<input type="text" placeholder="email"/>
-			<input type="text" placeholder="password"/>
-			<button>Submit</button>
-			<button>Cancel</button>
+			<h3>Enter in your information</h3>
+			<input placeholder="first name"
+			       onChange={handleFirst}/>
+			<input placeholder="last name"
+			       onChange={handleLast}/>
+			<input placeholder="email"
+			       onChange={handleEmail}/>
+			<input placeholder="password"
+			       onChange={handlePw}/>
+			<button onClick={handleSubmit}>Submit</button>
+			<button><Link to="/">Cancel</Link></button>
 		</section>
 	)
 }
