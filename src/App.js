1080468829d604ffd640de6c558ed6ada0e92130
Dallin Parker <dallin.r.parker@gmail.com>
2017-08-06 00:59:15 -0600

    working on adding profile image screenshots and facial recognition for login

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import {
   handleModal,
   handleChannelChange
 } from './redux/actions/navActions';
-
+let count = 0;
 class App extends Component {
   constructor(props) {
     super(props);
@@ -21,6 +21,7 @@ class App extends Component {
     this.handleCloseModal = this.handleCloseModal.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleProfileImg = this.handleProfileImg.bind(this);
+    this.takePhoto = this.takePhoto.bind(this);
   }
 
   handleAddChannel() {
@@ -34,15 +35,35 @@ class App extends Component {
   handleOpenModal = () => this.props.handleModal(true);
   handleCloseModal = () => this.props.handleModal(false);
 
+  takePhoto = () => {
+    console.log(count);
+    if (count > 1) {
+      const canvas = document.querySelector('canvas');
+      const context = canvas.getContext('2d');
+      let w = canvas.width;
+      let h = canvas.height;
+      let v = document.querySelector('video');
+      let p = document.getElementById('photo');
+      context.drawImage(v, 0, 0, w, h);
+      const data = canvas.toDataURL('image/png');
+      console.log('data: ', data);
+      p.setAttribute('src', data);
+      console.log('p: ', p);
+    }
+  };
+
   handleProfileImg = () => {
+    count++;
+
     //TODO: look into face detection software for logging in and adding funny effects to profile img
     const constraints = { audio: false, video: { width: 1280, height: 720 } };
     navigator.mediaDevices
       .getUserMedia(constraints)
       .then(mediaStream => {
+        console.log('mediastream: ', mediaStream);
         const video = document.querySelector('video');
         video.srcObject = mediaStream;
-        video.onloadedmetadata = e => video.play();
+        video.play();
       })
       .catch(err => console.log(`${err.name}: ${err.message}`));
   };
@@ -61,10 +82,29 @@ class App extends Component {
     return (
       <div className="main-container">
         <nav className="nav-container">
-          <video />
           <nav className="login-user">
-            <div className="profile-img" onClick={this.handleProfileImg} />
+            <div className="profile-img" onClick={this.handleProfileImg}>
+              <i
+                className="fa fa-user"
+                onClick={this.takePhoto}
+                aria-hidden="true"
+              >
+                {/* */}
+              </i>
+            </div>
             {this.props.currentUser}
+            <video />
+            <div className="snapshot">
+              <i className="fa fa-camera" aria-hidden="true">
+                {/* */}
+              </i>
+            </div>
+            <div className="output">
+              <img src classID="photo" alt="profile results" />
+            </div>
+            <canvas>
+              {/*screenshot*/}
+            </canvas>
           </nav>
           <nav id="nav-channels">
             <p onClick={this.handleOpenModal}>
