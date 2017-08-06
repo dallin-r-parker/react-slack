96d859e80021b56b27fef665c4f627e40b551a19
Dallin Parker <dallin.r.parker@gmail.com>
2017-08-06 02:00:46 -0600

    moving profile image into a component

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Nav from './components/Nav/Nav';
 import DirectChannels from './components/DirectChannels/DirectChannels';
 import CreateChannel from './components/CreateChannel/CreateChannel';
 import Messages from './components/MessagesLayout/MessagesView';
+import UserProfile from './components/UserProfile/UserProfile';
 import {
   addChannel,
   getChannels,
   handleModal,
   handleChannelChange
 } from './redux/actions/navActions';
+
 let count = 0;
 class App extends Component {
   constructor(props) {
@@ -20,8 +22,8 @@ class App extends Component {
     this.handleOpenModal = this.handleOpenModal.bind(this);
     this.handleCloseModal = this.handleCloseModal.bind(this);
     this.handleChange = this.handleChange.bind(this);
-    this.handleProfileImg = this.handleProfileImg.bind(this);
-    this.takePhoto = this.takePhoto.bind(this);
+    //this.handleProfileImg = this.handleProfileImg.bind(this);
+    //this.takePhoto = this.takePhoto.bind(this);
   }
 
   handleAddChannel() {
@@ -35,38 +37,39 @@ class App extends Component {
   handleOpenModal = () => this.props.handleModal(true);
   handleCloseModal = () => this.props.handleModal(false);
 
-  takePhoto = () => {
-    console.log(count);
-    if (count > 1) {
-      const canvas = document.querySelector('canvas');
-      const context = canvas.getContext('2d');
-      let w = canvas.width;
-      let h = canvas.height;
-      let v = document.querySelector('video');
-      let p = document.getElementById('photo');
-      context.drawImage(v, 0, 0, w, h);
-      const data = canvas.toDataURL('image/png');
-      console.log('data: ', data);
-      p.setAttribute('src', data);
-      console.log('p: ', p);
-    }
-  };
+  //takePhoto = () => {
+  //  //TODO: add a closure function for setting up initial state of screenshot
+  //  console.log(count);
+  //  if (count > 1) {
+  //    const canvas = document.querySelector('canvas');
+  //    const context = canvas.getContext('2d');
+  //    let w = canvas.width;
+  //    let h = canvas.height;
+  //    let v = document.querySelector('video');
+  //    let p = document.getElementById('photo');
+  //    context.drawImage(v, 0, 0, w, h);
+  //    const data = canvas.toDataURL('image/png');
+  //    console.log('data: ', data);
+  //    p.setAttribute('src', data);
+  //    console.log('p: ', p);
+  //  }
+  //};
 
-  handleProfileImg = () => {
-    count++;
-
-    //TODO: look into face detection software for logging in and adding funny effects to profile img
-    const constraints = { audio: false, video: { width: 1280, height: 720 } };
-    navigator.mediaDevices
-      .getUserMedia(constraints)
-      .then(mediaStream => {
-        console.log('mediastream: ', mediaStream);
-        const video = document.querySelector('video');
-        video.srcObject = mediaStream;
-        video.play();
-      })
-      .catch(err => console.log(`${err.name}: ${err.message}`));
-  };
+  //handleProfileImg = () => {
+  //  count++;
+  //
+  //  //TODO: look into face detection software for logging in and adding funny effects to profile img
+  //  const constraints = { audio: false, video: { width: 1280, height: 720 } };
+  //  navigator.mediaDevices
+  //    .getUserMedia(constraints)
+  //    .then(mediaStream => {
+  //      console.log('mediastream: ', mediaStream);
+  //      const video = document.querySelector('video');
+  //      video.srcObject = mediaStream;
+  //      video.play();
+  //    })
+  //    .catch(err => console.log(`${err.name}: ${err.message}`));
+  //};
 
   componentDidMount() {
     this.props.getChannels();
@@ -82,30 +85,30 @@ class App extends Component {
     return (
       <div className="main-container">
         <nav className="nav-container">
-          <nav className="login-user">
-            <div className="profile-img" onClick={this.handleProfileImg}>
-              <i
-                className="fa fa-user"
-                onClick={this.takePhoto}
-                aria-hidden="true"
-              >
-                {/* */}
-              </i>
-            </div>
-            {this.props.currentUser}
-            <video />
-            <div className="snapshot">
-              <i className="fa fa-camera" aria-hidden="true">
-                {/* */}
-              </i>
-            </div>
-            <div className="output">
-              <img src classID="photo" alt="profile results" />
-            </div>
-            <canvas>
-              {/*screenshot*/}
-            </canvas>
-          </nav>
+          {/*<nav className="login-user">*/}
+          {/*<div className="profile-img" onClick={this.handleProfileImg}>*/}
+          {/*<i*/}
+          {/*className="fa fa-user"*/}
+          {/*onClick={this.takePhoto}*/}
+          {/*aria-hidden="true"*/}
+          {/*>*/}
+          {/*/!* *!/*/}
+          {/*</i>*/}
+          {/*</div>*/}
+          {/*{this.props.currentUser}*/}
+          {/*<video />*/}
+          {/*<div className="snapshot">*/}
+          {/*<i className="fa fa-camera" aria-hidden="true">*/}
+          {/*/!* *!/*/}
+          {/*</i>*/}
+          {/*</div>*/}
+          {/*<div className="output">*/}
+          {/*<img classID="photo" alt="profile results" />*/}
+          {/*</div>*/}
+          {/*<canvas>*/}
+          {/*/!*screenshot*!/*/}
+          {/*</canvas>*/}
+          {/*</nav>*/}
           <nav id="nav-channels">
             <p onClick={this.handleOpenModal}>
               CHANNELS
