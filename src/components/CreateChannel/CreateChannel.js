6f39820c32c7f47a70768b6f0e3b7edae92cd5bc
Dallin Parker <dallin.r.parker@gmail.com>
2017-08-13 07:19:09 -0600

    added esc button when creating channel

--- src/components/CreateChannel/CreateChannel.js
+++ src/components/CreateChannel/CreateChannel.js
@@ -1,35 +1,56 @@
-import React from 'react'
-import './CreateChannel.css'
+import React from 'react';
+import './CreateChannel.css';
 
-export default function CreateChannel(props){
+export default function CreateChannel(props) {
+  props.value ? console.log('hasVal') : console.log('noVal: ');
+  console.log('propsChannel: ', props);
+  const handleCancel = () => props.action();
+  const handleCreate = () => props.add();
 
-	const handleCancel = () => (props.action())
-	const handleCreate = () => (props.add())
+  const channelInput = e => {
+    if (e.target.value) {
+      props.newChannel(e.target.value);
+    }
+  };
 
-	 const channelInput = (e) => {
-		if(e.target.value){
-			props.newChannel(e.target.value)
-		}
-	}
+  return (
+    <modal className="create-channel-container">
+      <section className="channel-wrap">
+        <div className="esc-wrap" onClick={handleCancel}>
+          <i className="fa fa-times" aria-hidden="true">
+            {/* */}
+          </i>
+          <esc>esc</esc>
+        </div>
+        <h2>Create a private channel</h2>
+        <h5>
+          Channels are where your team communicates. They're best when organized
+          around a topic - #leads, for example
+        </h5>
+        <p>Name</p>
+        <input
+          type="text"
+          onChange={e => channelInput(e)}
+          placeholder="e.g. leads"
+        />
 
-	return (
-		<modal className="create-channel-container">
-			<section className="channel-wrap">
-				<h2>Create a private channel</h2>
-				<h5>Channels are where your team communicates. They're best when organized around a topic - #leads, for example</h5>
-				<p>Name</p>
-				<input type="text"
-				       onChange={e => channelInput(e)}
-				       placeholder="e.g. leads" />
+        <p>Purpose</p>
+        <input type="text" />
 
-				<p>Purpose</p>
-				<input type="text"/>
-
-				<div className="btn-wrap">
-					<button onClick={handleCancel}>Cancel</button>
-					<button onClick={handleCreate}>Create channel</button>
-				</div>
-			</section>
-		</modal>
-	)
+        <div className="btn-wrap">
+          <button onClick={handleCancel}>Cancel</button>
+          <button
+            onClick={handleCreate}
+            style={
+              props.value
+                ? { background: '#2AB27B' }
+                : { background: '#CDCECE' }
+            }
+          >
+            Create channel
+          </button>
+        </div>
+      </section>
+    </modal>
+  );
 }
